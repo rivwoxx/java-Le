3af16a1c3c0a6cf9b1a8c607e63e201db917f513
rivwoxx <rivera_augusto@protonmail.com>
2021-08-02 01:06:22 -0500

    same exercise but can verify more that one and stuff

--- Exercises/src/exercises/DNA.java
+++ Exercises/src/exercises/DNA.java
@@ -1,27 +1,47 @@
 package exercises;
 
+import java.util.ArrayList;
+
 public class DNA {
 	 
 	  public static void main(String[] args) {
 	    
-	    String dna1 = "ATGCGATACGCTTGA";
-	    String dna2 = "ATGCGATACGTGA";
-	    String dna3 = "ATTAATATGTACTGA";
+		ArrayList<String> dnas = new ArrayList<String>();
+		  
+	    dnas.add("ATGCGATACGCTTGA"); 
+	    dnas.add("ATGCGATACGTGA");
+	    dnas.add("ATTAATATGTACTGA");
+	    dnas.add("ATGCGATACGCTTGA");
 
-	    String dna = dna3;
-	    System.out.println(dna.length());
+	    //String dna = dna3;
+	    //System.out.println(dna.length());
 
-	    int atg = dna.indexOf("ATG");
-	    int tga = dna.indexOf("TGA");
-
-	    System.out.println(dna.indexOf("ATG"));
-	    System.out.println(dna.indexOf("TGA"));
-
-	    if(atg == 0 && tga == 12 && (tga - atg) % 3 == 0){
-	      System.out.println("There is a protein");
-	      String protein = dna.substring(atg,tga);
-	    }else{
-	      System.out.println("There is NOT a protein");
+//	    int atg = dna.indexOf("ATG");
+//	    int tga = dna.indexOf("TGA");
+//
+//	    System.out.println(dna.indexOf("ATG"));
+//	    System.out.println(dna.indexOf("TGA"));
+//
+//	    if(atg == 0 && tga == 12 && (tga - atg) % 3 == 0){
+//	      System.out.println("There is a protein");
+//	      String protein = dna.substring(atg,tga);
+//	    }else{
+//	      System.out.println("There is NOT a protein");
+//	    }
+	    
+	    for(int i = 0; i < dnas.size(); i++) {
+	    	String dna = dnas.get(i);
+	    	
+	    	int atg = dna.indexOf("ATG");
+	    	int tga = dna.indexOf("TGA");
+	    	
+	    	if(atg == 0 && tga == 12 && (tga - atg) % 3 == 0){
+	  	      System.out.println("There is a protein");
+	  	      String protein = dna.substring(atg,tga);
+	  	      System.out.println(protein);
+	  	    }else{
+	  	      System.out.println("There is NOT a protein");
+	  	    }
 	    }
 
 	  }
